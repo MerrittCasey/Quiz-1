42247ee96774b6e9e2f5fce379f6aaa8e1d1d21d
TheZarvin <46637768+TheZarvin@users.noreply.github.com>
2022-03-02 17:55:58 -0800

    added two files and outlines

---
+++ guessingGame.c
@@ -0,0 +1,42 @@
+/*
+    Outline:
+    
+    main(){
+        read max from file and save to local variable
+
+        output menu options
+
+        scan for user input
+
+        if user input is 1 call playGame()
+
+        if user input is 2 call changeMax()
+
+        if user input is 3 output thank you message and respective game info
+
+        save data to file
+
+        close file
+    }
+
+    void playGame(){
+        generate random number between 1 and max value
+
+        ask for user input
+
+        check if user input is "q" otherwise compare to generated number
+
+        if input is wrong then repeat keep track of total games played while going
+
+    }
+
+    void changeMax(){
+        ask for user input 
+
+        if user input isn't to large for int to handle
+
+        change max variable
+
+    }
+
+*/
---
+++ restaurant.c
@@ -0,0 +1,23 @@
+/*
+    Outline:
+    
+    main(){
+        get random number between 1 and 4
+        use random number to determine meal type and cost
+
+        declare float values for tip and tax
+
+        scan for input
+
+        check to make sure input is greater than 0
+
+        divide input by 100
+
+        multiply meal cost by tip amount and add to meal cost
+
+        multiply total by tax amount and add to total
+
+        print total
+    }
+
+*/
