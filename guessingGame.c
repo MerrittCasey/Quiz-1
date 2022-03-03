579a4f36bc687a5532f9be119a84baca6c110e9a
TheZarvin <46637768+TheZarvin@users.noreply.github.com>
2022-03-02 20:48:59 -0800

    added functioning code to both

--- guessingGame.c
+++ guessingGame.c
@@ -40,3 +40,168 @@
     }
 
 */
+#include <string.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void playGame();
+void changeMax();
+
+int max;
+int numberOfGames = 0;
+int wins[10];
+int trys[10];
+
+int main(){
+    for(int i =0; i < 10; i++){
+        trys[i] = 0;
+    }
+    
+    FILE* ifile = fopen("save_user_max_number.txt", "r");
+
+    fscanf(ifile, "%d", &max);
+
+    fclose(ifile);
+
+    printf("WELCOME TO GUESSING GAME\n");
+
+    int cont = 1;
+
+    while(cont == 1){
+        printf("\n");
+        printf("Press 1 to play a game\n");
+        printf("Press 2 to change the max number\n");
+        printf("Press 3 to quit\n");
+
+        int input;
+        scanf("%d", &input);
+
+        if(input == 1){
+            playGame();
+        }else if(input == 2){
+            changeMax();
+        }else if(input == 3){
+            printf("\n");
+            printf("Thanks for playing!\n");
+            printf("\n");
+            printf("Here are your results\n");
+            printf("---------------------\n");
+
+            for(int i = 0; i < 10; i++){
+                if(trys[i] != 0){
+                    if(wins[i] == 0){
+                        printf("You lost game %d with %d try(s)\n", i + 1, trys[i]);
+                    }else if(wins[i] == 1){
+                        printf("You won game %d with %d try(s)\n", i + 1, trys[i]);
+                    }
+                }
+            }
+            cont = 0;
+        }else{
+            printf("Invalid input pls try again!\n");
+        }
+    }
+
+    FILE* ofile = fopen("save_user_max_number.txt", "w");
+    fprintf(ofile, "%d", max);
+    fclose(ofile);
+
+    return 0;
+}
+
+void playGame(){
+    printf("\n");
+    printf("LETS PLAY!\n");
+
+    time_t t;
+    srand((unsigned) time(&t));
+    int random = (rand() % max) + 1;
+
+    printf("Generated a random number between 1 and %d\n", max);
+    printf("\n");
+
+    int cont = 1;
+    int count = 0;
+
+    while(cont == 1){
+        char str[10];
+        int quit = 0;
+        int guess = 0;
+        printf("Take a guess: ");
+        scanf("%s", &str[0]);
+        
+        for(int i = 0; i < 10; i++){
+            if(str[0] == 113){
+                quit = 1;
+                break;
+            }
+
+            guess = atoi(str);
+        }
+
+        if(quit == 1){
+            printf("\n");
+            printf("Well you lose then quitter!\n");
+            count++;
+            for(int i = 0; i < 10; i++){
+                if(trys[i] == 0){
+                    trys[i] = count;
+                    wins[i] = 0;
+                    break;
+                }
+            }
+
+            numberOfGames++;
+            cont = 0;
+        }else{
+            if(guess == random){
+                count++;
+                printf("\n");
+                printf("Congratulations you got it!\n");
+                printf("It only took you %d trys!\n", count);
+
+                for(int i = 0; i < 10; i++){
+                    if(trys[i] == 0){
+                        trys[i] = count;
+                        wins[i] = 1;
+                        break;
+                    }
+                }   
+
+                numberOfGames++;
+                cont = 0;
+            }else if(guess < random){
+                printf("Wrong your too low!\n");
+                count++;
+            }else if(guess > random){
+                printf("Wrong your too high!\n");
+                count++;
+            }
+        }
+    }
+}
+
+void changeMax(){
+    printf("\n");
+    printf("Lets change the max value!\n");
+
+    int newMax;
+    int cont = 1;
+
+    while(cont == 1){
+        printf("\n");
+        printf("Enter a number between 1 and 2,147,483,647: ");  
+        scanf("%d", &newMax);
+
+        if(newMax < 1){
+            printf("Invalid input try again\n");
+        }else{
+            max = newMax;
+            printf("max value changed!\n");
+            cont = 0;
+        }
+
+    }
+}
--- restaurant.c
+++ restaurant.c
@@ -21,3 +21,75 @@
     }
 
 */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(){
+    float mealCost = 0;
+    
+    time_t t;
+    srand((unsigned) time(&t));
+    int random = (rand() % 4) + 1;
+
+    if(random == 1){
+        mealCost = 9.95;
+    }else if(random == 2){
+        mealCost = 4.55;
+    }else if(random == 3){
+        mealCost = 13.25;
+    }else if(random == 4){
+        mealCost = 22.35;
+    }
+
+    float tip;
+    float tax;
+    float total = mealCost;
+
+    int cont = 1;
+
+    while(cont == 1){
+        printf("Enter tip percentage: ");
+        scanf("%f", &tip);
+
+        if(tip < 0 ){
+            printf("Tip must be greater than or equal to 0");
+            printf("\n");
+        }else{
+            tip = (tip / 100);
+            tip = (total * tip);
+            total = (total + tip);
+
+            cont = 0;
+        }
+
+    }
+    printf("\n");
+    
+    cont = 1;
+    
+    while(cont == 1){
+        printf("Enter tax percentage: ");
+        scanf("%f", &tax);
+
+        if(tax < 0 ){
+            printf("Tax must be greater than or equal to 0");
+            printf("\n");
+        }else{
+            tax = (tax / 100);
+            tax = (total * tax);
+            total = (total + tax);
+
+            cont = 0;
+        }
+
+    }
+    printf("\n");
+    
+    printf("Meal Cost Before Tax and Tip: $%.2f\n", mealCost);
+    printf("Tip amount: $%.2f\n", tip);
+    printf("Tax amount: $%.2f\n", tax);
+    printf("Total MealCost: $%.2f\n", total);
+
+    return 0;
+}
